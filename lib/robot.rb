d46eae9cc49ced3f6e8f8fdd817ccbba7265548f
NJBLAGA <nathanblaga90@gmail.com>
2021-07-31 20:45:38 +1000

    add robot class, created place,move,left,right,report methods

---
+++ lib/robot.rb
@@ -0,0 +1,94 @@
+require './board.rb'
+
+class Robot
+    attr_reader :board
+
+        def initialize(setting_robot={})
+            @positionX = setting_robot[:positionX]
+            @positionY = setting_robot[:positionY]
+            @direction = setting_robot[:direction]
+            @bearings = setting_robot[:direction] = ["NORTH", "SOUTH", "EAST", "WEST"]
+            @tile = setting_robot[:tile]
+        end
+
+        def place(x,y,direction)
+            @positionX = x
+            @positionY = y
+            @direction = direction
+                if @bearings.include? @direction
+                   @direction
+                else
+                   puts "Invalid direction -- TRY AGAIN"
+                end
+                create_board_hash.each do |key, value|
+                    if value[:x] == @positionX.to_s && value[:y]  == @positionY.to_s
+                       @tile = key
+                    else
+                       #  puts "POSITION NOT FOUND -- TRY AGAIN"
+                    end
+                end
+                # puts "Set Robot Position at #{@tile} - PositionX: #{x}, PositionY: #{y}, Facing: #{direction}"
+        end
+
+        def move
+            case @direction
+                when "NORTH"
+                    @positionY +=1
+                when "SOUTH"
+                    @positionY -=1
+                when "EAST"
+                    @positionX +=1
+                when "WEST"
+                    @positionX -=1
+                else
+                    puts "INVALID MOVE"
+                end
+                # puts "Robot Will MOVE at PositionX: #{@positionX}, PositionY: #{@positionY}, Facing: #{@direction}"
+                place(@positionX,@positionY,@direction)
+        end
+
+        def left
+            case @direction
+                when "NORTH"
+                    @direction = "WEST"
+                when "SOUTH"
+                    @direction = "EAST"
+                when "EAST"
+                    @direction = "NORTH"
+                when "WEST"
+                    @direction = "SOUTH"
+                else
+             end
+                # puts " Robot is now FACING #{@direction}"
+        end
+
+        def right
+            case @direction
+                when "NORTH"
+                    @direction = "EAST"
+                when "SOUTH"
+                    @direction = "WEST"
+                when "EAST"
+                    @direction = "SOUTH"
+                when "WEST"
+                    @direction = "NORTH"
+                else
+            end
+            # puts " Robot is now FACING #{@direction}"
+        end
+
+        def report
+            puts "Set Robot Position at #{@tile} - PositionX: #{@positionX}, PositionY: #{@positionY}, Facing: #{@direction}"
+            puts display_board(create_board_hash)
+        end
+
+end
+
+puts display_board(create_board_hash)
+robot1 = Robot.new
+robot1.place(1,2,"EAST")
+robot1.move
+robot1.move
+robot1.left
+robot1.move
+robot1.report
